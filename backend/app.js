cdd7ce74a8ae39f701fdb5cb1059d027dd94ea9c
Narayandwivedi <desinplus1@gmail.com>
2026-02-26 18:35:43 +0530

    fix cors error

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,8 @@ const allowedOrigins = [
     "http://localhost:5174",
     "http://localhost:5175",
     "http://localhost:5176",
+    "https://abcdservices.online",
+    "https://api.abcdservices.online"
 ];
 
 app.use(cors({
